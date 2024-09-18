dfdcd42106b63563932489aeb42d6231d1521561
cornel12-hen <noelmarvell6@gmail.com>
2024-09-18 10:00:07 +0700

    JS

--- asset/js/sign_in.js
+++ asset/js/sign_in.js
@@ -1,16 +1,16 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const correctEmail = "Techfree.gmail.com";
-    const correctPassword = "Techfree123";
+  const correctEmail = "Techfree@gmail.com";
+  const correctPassword = "Techfree123";
 
-    const inputEmail = document.getElementById('email').value;
-    const inputPassword = document.getElementById('password').value;
+  const inputEmail = document.getElementById("email").value;
+  const inputPassword = document.getElementById("password").value;
 
-    if (inputEmail === correctEmail && inputPassword === correctPassword) {
-        alert("Login Berhasil");
-        window.location.href = 'index.html';
-    } else {
-        alert("Email atau Password salah");
-    }
+  if (inputEmail === correctEmail && inputPassword === correctPassword) {
+    alert("Login Berhasil");
+    window.location.href = "index.html";
+  } else {
+    alert("Email atau Password salah");
+  }
 });
